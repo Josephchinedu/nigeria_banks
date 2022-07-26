620a67cce259c897e6e1579b8ba6325f8476f7cd
Joseph chinedu <josephchinedu34@gmail.com>
2022-07-26 16:00:02 +0100

    First deployment

---
+++ README.md
@@ -0,0 +1,34 @@
+📦 nigeria_banks
+=======================
+
+Cbn Banks is a basic python package that returns details of particular bank in Nigeria.
+
+## Installation
+
+You can install Cbn Bankss from [PyPI](https://pypi.org/project/nigeria_banks/):
+
+    pip install nigeria_banks
+
+
+
+## How to use
+
+    $ from nigeria_banks import bank
+        # get bank details using ussd_code
+        get_bank = getBank(ussd_code="*945#")
+        # result = {'bank_code': '000017', 'cbn_code': '035', 'name': 'Wema Bank', 'bank_short_name': 'wema', 'disabled_for_vnuban': 'None', 'ussd_code': '*945#'}
+
+        # get bank details using bank_code
+        get_bank = getBank(bank_code="000017")
+        # result = {'bank_code': '000017', 'cbn_code': '035', 'name': 'Wema Bank', 'bank_short_name': 'wema', 'disabled_for_vnuban': 'None', 'ussd_code': '*945#'}
+
+        # get bank details using cbn_code
+        get_bank = getBank(cbn_code="035")
+        # result =  {'bank_code': '000017', 'cbn_code': '035', 'name': 'Wema Bank', 'bank_short_name': 'wema', 'disabled_for_vnuban': 'None', 'ussd_code': '*945#'}
+
+        # get bank details using name
+        get_bank = getBank(name="wema")
+        # result = {'bank_code': '000017', 'cbn_code': '035', 'name': 'Wema Bank', 'bank_short_name': 'wema', 'disabled_for_vnuban': 'None', 'ussd_code': '*945#'}
+
+     
+
---
+++ test.py
@@ -0,0 +1,48 @@
+from nigeria_banks.database.db import bankdb
+
+
+banks = [
+    {'name': 'ACCESS BANK PLC', 'code': '044', 'slug': 'ACC', 'ussd_code': '*901#'},
+    {'name': 'CITIBANK NIGERIA PLC', 'code': '023', 'slug': 'CBN', 'ussd_code': None},
+    {'name': 'DIAMOND BANK PLC', 'code': '063', 'slug': 'DMB', 'ussd_code': '*710#'},
+    {'name': 'ECOBANK NIGERIA PLC', 'code': '050', 'slug': 'EBN', 'ussd_code': '*326#'},
+    {'name': 'ENTERPRISE BANK', 'code': '084', 'slug': 'EPB', 'ussd_code': '*901#'},
+    {'name': 'FIDELITY BANK PLC', 'code': '070', 'slug': 'FDB', 'ussd_code': '*770#'},
+    {'name': 'FIRST BANK NIGERIA LIMITED', 'code': '011', 'slug': 'FBN', 'ussd_code': '*894#'},
+    {'name': 'FIRST CITY MONUMENT BANK PLC', 'code': '214', 'slug': 'FCB', 'ussd_code': '*329#'},
+    {'name': 'GUARANTY TRUST BANK PLC', 'code': '058', 'slug': 'GTB', 'ussd_code': '*737#'},
+    {'name': 'HERITAGE BANK PLC', 'code': '030', 'slug': 'HTB', 'ussd_code': '*322#'},
+    {'name': 'KEY STONE BANK', 'code': '082', 'slug': 'KSB', 'ussd_code': '*533#'},
+    {'name': 'MAINSTREET BANK', 'code': '014', 'slug': 'MSB', 'ussd_code': None},
+    {'name': 'POLARIS BANK LIMITED', 'code': '076', 'slug': 'PLB', 'ussd_code': None},
+    {'name': 'PROVIDUS BANK LIMITED', 'code': '101', 'slug': 'PVB', 'ussd_code': None},
+    {'name': 'STANBIC IBTC BANK LTD', 'code': '221', 'slug': 'SIB', 'ussd_code': '*909#'},
+    {
+        'name': 'STANDARD CHARTERED BANK NIGERIA LTD',
+        'code': '068', 'slug': 'SCB', 'ussd_code': None
+    },
+    {'name': 'STERLING BANK PLC', 'code': '232', 'slug': 'STB', 'ussd_code': '*822#'},
+    {'name': 'SUNTRUST BANK NIGERIA LTD', 'code': '100', 'slug': 'SBN', 'ussd_code': None},
+    {'name': 'UNION BANK OF NIGERIA PLC', 'code': '032', 'slug': 'UBN', 'ussd_code': '*826#'},
+    {'name': 'UNITED BANK FOR AFRICA PLC', 'code': '033', 'slug': 'UBA', 'ussd_code': '*919#'},
+    {'name': 'UNITY BANK PLC', 'code': '215', 'slug': 'UNB', 'ussd_code': '*7799#'},
+    {'name': 'WEMA BANK PLC', 'code': '035', 'slug': 'WEM', 'ussd_code': '*945#'},
+    {'name': 'ZENITH BANK PLC', 'code': '057', 'slug': 'ZIB', 'ussd_code': '*966#'}
+]
+
+
+main_bank = bankdb()
+
+data = []
+
+for i in main_bank:
+    for e in banks:
+        if i['cbn_code'] == e['code']:
+            i["ussd_code"] = e["ussd_code"]
+            data.append(i)
+
+
+
+print(data)
+
+            
