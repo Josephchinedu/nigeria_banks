537c172a8157285a07340a6ef72cd3a757acb499
Joseph chinedu <josephchinedu34@gmail.com>
2022-07-26 16:02:45 +0100

    branched name update on setup file

---
+++ cbn_banks/bank.py
@@ -0,0 +1,35 @@
+from nigeria_banks.database.db import bankdb
+
+
+def getBank(name=None, bank_code=None, cbn_code=None, ussd_code = None):
+    data = bankdb()
+    if name:
+        for bank in data:
+            if bank["bank_short_name"] == name:
+                return bank
+    elif bank_code:
+        for bank in data:
+            if bank["bank_code"] == bank_code:
+                return bank
+    elif cbn_code:
+        for bank in data:
+            if bank["cbn_code"] == cbn_code:
+                return bank
+    elif ussd_code:
+        if "*" not in ussd_code or "#" not in ussd_code:
+            return None
+        for bank in data:
+            if bank["ussd_code"] == ussd_code:
+                return data
+    return None
+
+
+def getBanks():
+    """ Method to return all the banks in Nigeria. """
+    return bankdb()
+
+
+
+
+
+
---
+++ cbn_banks/database/db.py
@@ -0,0 +1,181 @@
+def bankdb():
+    data = [
+        {
+            "bank_code":"000014",
+            "cbn_code":"044",
+            "name":"Access Bank Nigeria",
+            "bank_short_name":"access",
+            "disabled_for_vnuban": False,
+            "ussd_code":"*901#"
+        },
+        {
+            "bank_code":"000014",
+            "cbn_code":"063",
+            "name":"DIAMOND BANK PLC",
+            "bank_short_name":"diamond",
+            "disabled_for_vnuban": False,
+            "ussd_code":"*710#"
+        },
+        {
+            "bank_code":"000010",
+            "cbn_code":"050",
+            "name":"Ecobank Bank",
+            "bank_short_name":"ecobank",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*326#"
+        },
+        {
+            "bank_code":"000007",
+            "cbn_code":"070",
+            "name":"Fidelity Bank",
+            "bank_short_name":"fidelity",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*770#"
+        },
+        {
+            "bank_code":"000016",
+            "cbn_code":"011",
+            "name":"First Bank of Nigeria",
+            "bank_short_name":"firstbank",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*894#"
+        },
+        {
+            "bank_code":"000003",
+            "cbn_code":"214",
+            "name":"First City Monument Bank",
+            "bank_short_name":"fcmb",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*329#"
+        },
+        {
+            "bank_code":"000013",
+            "cbn_code":"058",
+            "name":"Guaranty Trust Bank",
+            "bank_short_name":"gtb",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*737#"
+        },
+        {
+            "bank_code":"000020",
+            "cbn_code":"030",
+            "name":"Heritage Bank",
+            "bank_short_name":"heritage",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*322#"
+        },
+        {
+            "bank_code":"000002",
+            "cbn_code":"082",
+            "name":"Keystone Bank",
+            "bank_short_name":"keystone",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*533#"
+        },
+        {
+            "bank_code":"000008",
+            "cbn_code":"076",
+            "name":"Polaris Bank",
+            "bank_short_name":"PBL",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"None"
+        },
+        {
+            "bank_code":"000001",
+            "cbn_code":"232",
+            "name":"Sterling Bank",
+            "bank_short_name":"",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*822#"
+        },
+        {
+            "bank_code":"000018",
+            "cbn_code":"032",
+            "name":"Union Bank of Nigeria",
+            "bank_short_name":"unionbank",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*826#"
+        },
+        {
+            "bank_code":"000004",
+            "cbn_code":"033",
+            "name":"United Bank for Africa",
+            "bank_short_name":"uba",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*919#"
+        },
+        {
+            "bank_code":"000011",
+            "cbn_code":"215",
+            "name":"Unity Bank",
+            "bank_short_name":"unity",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*7799#"
+        },
+        {
+            "bank_code":"000017",
+            "cbn_code":"035",
+            "name":"Wema Bank",
+            "bank_short_name":"wema",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*945#"
+        },
+        {
+            "bank_code":"000015",
+            "cbn_code":"057",
+            "name":"Zenith Bank",
+            "bank_short_name":"zenith",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*966#"
+        },
+        {
+            "bank_code":"000015",
+            "cbn_code":"057",
+            "name":"Zenith Bank",
+            "bank_short_name":"zenith",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*966#"
+        },
+        {
+            "bank_code":"000022",
+            "cbn_code":"100",
+            "name":"Suntrust Bank",
+            "bank_short_name":"",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"None"
+        },
+        {
+            "bank_code":"000021",
+            "cbn_code":"068",
+            "name":"Standard Chartered Bank",
+            "bank_short_name":"standard-chartered",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"None"
+        },
+        {
+            "bank_code":"000012",
+            "cbn_code":"221",
+            "name":"Stanbic IBTC",
+            "bank_short_name":"stanbic",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"*909#"
+        },
+        {
+            "bank_code":"000023",
+            "cbn_code":"101",
+            "name":"Providus Bank",
+            "bank_short_name":"providus",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"None"
+        },
+        {
+            "bank_code":"000009",
+            "cbn_code":"023",
+            "name":"Citibank Nigeria",
+            "bank_short_name":"citibankng",
+            "disabled_for_vnuban":"None",
+            "ussd_code":"None"
+        }
+    ]
+
+    return data
